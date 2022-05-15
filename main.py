a32a8a5a5a746124456015f77e38a8a50a0cc27b
Joe Hua <71626151+Gombessa1938@users.noreply.github.com>
2022-05-15 16:00:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,27 @@ mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transf
 down_sampled_train = torch.from_numpy(np.load('down_sampled_train.npy'))
 concat_train = torch.from_numpy(np.load('concat_train.npy'))
 
-model1 = small()
-loss = nn.NLLLoss(reduction='none')
-optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-train(model1,loss,optim,down_sampled_train,mnist_trainset.targets,300,128)
+def cycle_train(epoch1,epoch2,epoch3,counter):
+	'''
+	complete one training cycle
+	small network -> large network -> small network 
+	'''		
+	model1 = small()
+	model2 = Large()
+	load_model_weight(model1,model2,small_to_big=False)
+	loss = nn.NLLLoss(reduction='none')
+	optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
+	train(model1,loss,optim,down_sampled_train,mnist_trainset.targets,epoch1,128)
 
-model2 = Large()
-load_model_weight(model1,model2,small_to_big=True)
-optim = torch.optim.Adam(model2.parameters(), lr=0.001)
-train(model2,loss,optim,concat_train,mnist_trainset.targets,100,128)
+	model2 = Large()
+	load_model_weight(model1,model2,small_to_big=True)
+	optim = torch.optim.Adam(model2.parameters(), lr=0.001)
+	train(model2,loss,optim,concat_train,mnist_trainset.targets,epoch2,128)
 
-model1 = small()
-load_model_weight(model2,model1,small_to_big=False)
-loss = nn.NLLLoss(reduction='none')
-optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-train(model1,loss,optim,down_sampled_train,mnist_trainset.targets,300,128)
+	model1 = small()
+	load_model_weight(model2,model1,small_to_big=False)
+	loss = nn.NLLLoss(reduction='none')
+	optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
+	train(model1,loss,optim,down_sampled_train,mnist_trainset.targets,epoch3,128)
+
+cycle_train(50,50,50,counter = 1)
