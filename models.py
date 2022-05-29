b93e4218732bf24866305292ef9832f4e18a6240
Joe Hua <71626151+Gombessa1938@users.noreply.github.com>
2022-05-29 11:54:42 -0700

    Update models.py

--- models.py
+++ models.py
@@ -5,16 +5,17 @@ import torch.nn.functional as F
 class Large(torch.nn.Module):
   '''
   take in concat(full size differene image(28x28), downsampled image(14x14))
-
   '''
   def __init__(self):
     super(Large, self).__init__()
-    self.l1 = nn.Linear(980, 128, bias=False) #28*28 + 14*14  ---> 128, 128 --->10
-    self.l2 = nn.Linear(128, 10, bias=False)
+    self.l1 = nn.Linear(980, 256, bias=False) #28*28 + 14*14  ---> 128, 128 --->10
+    self.l2 = nn.Linear(256, 20, bias=False)
+    self.l3 = nn.Linear(20,10,bias=False)
     self.sm = nn.LogSoftmax(dim=1)
   def forward(self, x):
     x = F.relu(self.l1(x))
     x = self.l2(x)
+    x = self.l3(x)
     x = self.sm(x)
     return x
 
@@ -25,10 +26,12 @@ class small(torch.nn.Module):
   def __init__(self):
     super(small, self).__init__()
     self.l1 = nn.Linear(14*14, 128, bias=False)  # 196-->128, 128-->10
-    self.l2 = nn.Linear(128, 10, bias=False)
+    self.l2 = nn.Linear(128, 20, bias=False)
+    self.l3 = nn.Linear(20,10,bias=False)
     self.sm = nn.LogSoftmax(dim=1)
   def forward(self, x):
     x = F.relu(self.l1(x))
     x = self.l2(x)
+    x = self.l3(x)
     x = self.sm(x)
     return x
