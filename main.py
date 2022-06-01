430985bd9c6d469682548c0501d5c0bf788a45bf
Joe Hua <71626151+Gombessa1938@users.noreply.github.com>
2022-06-01 13:27:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,28 @@ mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transf
 down_sampled_train = torch.from_numpy(np.load('down_sampled_train.npy'))
 concat_train = torch.from_numpy(np.load('concat_train.npy'))
 
-def cycle_train(epoch1,epoch2,epoch3,counter):
+def cycle_train(epoch1,epoch2,epoch3,cycle):
 	'''
 	complete one training cycle
 	small network -> large network -> small network 
 	'''		
 	model1 = small()
 	model2 = Large()
-	load_model_weight(model1,model2,small_to_big=False)
-	loss = nn.NLLLoss(reduction='none')
-	optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-	train(model1,loss,optim,down_sampled_train,mnist_trainset.targets,epoch1,128)
+	for i in range(cycle):
+		load_model_weight(model1,model2,small_to_big=False)
+		loss = nn.NLLLoss(reduction='none')
+		optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
+		train(model1,loss,optim,down_sampled_train,mnist_trainset.targets,epoch1,128)
 
-	model2 = Large()
-	load_model_weight(model1,model2,small_to_big=True)
-	optim = torch.optim.Adam(model2.parameters(), lr=0.001)
-	train(model2,loss,optim,concat_train,mnist_trainset.targets,epoch2,128)
+		model2 = Large()
+		load_model_weight(model1,model2,small_to_big=True)
+		optim = torch.optim.Adam(model2.parameters(), lr=0.001)
+		train(model2,loss,optim,concat_train,mnist_trainset.targets,epoch2,128)
 
-	model1 = small()
-	load_model_weight(model2,model1,small_to_big=False)
-	loss = nn.NLLLoss(reduction='none')
-	optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-	train(model1,loss,optim,down_sampled_train,mnist_trainset.targets,epoch3,128)
+		model1 = small()
+		load_model_weight(model2,model1,small_to_big=False)
+		loss = nn.NLLLoss(reduction='none')
+		optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
+		train(model1,loss,optim,down_sampled_train,mnist_trainset.targets,epoch3,128)
 
-cycle_train(50,50,50,counter = 1)
+cycle_train(20,20,20,cycle=2)
