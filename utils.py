e27477585e1d8c9715d74329751c9d44e2042ad1
Joe Hua <71626151+Gombessa1938@users.noreply.github.com>
2022-06-01 13:28:14 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,12 +10,18 @@ def load_model_weight(model1,model2,small_to_big = True):
 		model1_l1_weight = model1.l1.weight.clone().detach()
 		model1_l2_weight = model1.l2.weight.clone().detach()
 		model1_l3_weight = model1.l3.weight.clone().detach()
+		#print(model1.l1.weight.grad.cpu().detach().numpy())
+		
+		#concat first layer weight
 		zero = torch.zeros(128,784)
 		concat_weight_second_half = torch.zeros(128,980)
 		concat_weight_first_half = torch.cat((zero,model1_l1_weight),dim=1)
+		
+		#concat second layer weight
 		concat = torch.cat((concat_weight_second_half,concat_weight_first_half),dim=0)
 		l2_zero = torch.zeros(20,128)
 		concat_l2 = torch.cat((l2_zero,model1_l2_weight),dim=1)
+		
 		model2.l1.weight = torch.nn.Parameter(concat)
 		model2.l2.weight = torch.nn.Parameter(concat_l2)
 		model2.l3.weight = torch.nn.Parameter(model1_l3_weight)
@@ -29,3 +35,4 @@ def load_model_weight(model1,model2,small_to_big = True):
 		model_new_small.l1.weight = torch.nn.Parameter(new_l1_weight)
 		model_new_small.l2.weight = torch.nn.Parameter(new_l2_weight)
 		model_new_small.l3.weight = torch.nn.Parameter(model1.l3.weight.clone().detach())
+
