1b30a54f18b3454a913cf88d0fcf75f1f1bc5c27
Joe Hua <infomrhua@gmail.com>
2022-06-03 09:59:25 -0700

    modification

--- main.py
+++ main.py
@@ -8,47 +8,32 @@ from train import train
 from utils import load_model_weight
 
 #data loading
-data = np.load('/Users/joe/Documents/llnl.npz')
+data = np.load('/Users/joe/Documents/llnl_branch/llnl.npz')
 label = torch.from_numpy(data['Q'].astype('float32'))
-down_sampled_train = torch.from_numpy(np.load('down_sampled_train.npy'))
-concat_train = torch.from_numpy(np.load('concat_train.npy'))
+down_sampled_train = torch.from_numpy(np.load('/Users/joe/Documents/llnl_branch/down_sampled_train.npy'))
+concat_train = torch.from_numpy(np.load('/Users/joe/Documents/llnl_branch/concat_train.npy'))
 
-def cycle_train(epoch1,epoch2,epoch3,counter):
-	'''
-	complete one training cycle
-	small network -> large network -> small network 
-	'''		
-	model1 = small()
-	model2 = Large()
-	#load_model_weight(model1,model2,small_to_big=False)
-	loss = nn.MSELoss()
-	optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-	train(model1,loss,optim,down_sampled_train,label,epoch1,128)
+def cycle_train(epoch1,epoch2,epoch3,cycle):
+    '''
+    complete one training cycle
+    small network -> large network -> small network 
+    '''		
+    model1 = small()
+    model2 = Large()
+    for i in range(cycle):
+        load_model_weight(model1,model2,small_to_big=False)
+        loss = nn.MSELoss()
+        optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
+        train(model1,loss,optim,down_sampled_train,label,epoch1,128)
 
-	model2 = Large()
-	load_model_weight(model1,model2,small_to_big=True)
-	optim = torch.optim.Adam(model2.parameters(), lr=0.0001)
-	train(model2,loss,optim,concat_train,label,epoch2,128)
+        model2 = Large()
+        load_model_weight(model1,model2,small_to_big=True)
+        optim = torch.optim.Adam(model2.parameters(), lr=0.0001)
+        train(model2,loss,optim,concat_train,label,epoch2,128)
 
-	model1 = small()
-	load_model_weight(model2,model1,small_to_big=False)
-	optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-	train(model1,loss,optim,down_sampled_train,label,epoch3,128)
- 
-#===============================
+        model1 = small()
+        load_model_weight(model2,model1,small_to_big=False)
+        optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
+        train(model1,loss,optim,down_sampled_train,label,epoch3,128)
 
-	load_model_weight(model1,model2,small_to_big=False)
-	optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-	train(model1,loss,optim,down_sampled_train,label,10,128)
-
-	model2 = Large()
-	load_model_weight(model1,model2,small_to_big=True)
-	optim = torch.optim.Adam(model2.parameters(), lr=0.0001)
-	train(model2,loss,optim,concat_train,label,10,128)
-
-	model1 = small()
-	load_model_weight(model2,model1,small_to_big=False)
-	optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-	train(model1,loss,optim,down_sampled_train,label,10,128)
-
-cycle_train(50,8,50,counter = 1)
+cycle_train(50,50,50,cycle=2)
