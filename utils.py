eebf6bff56163b6c017057f0759f7dc8ecc03032
Joe Hua <infomrhua@gmail.com>
2022-06-11 11:10:02 -0700

    .

--- utils.py
+++ utils.py
@@ -28,23 +28,6 @@ def load_model_weight(model1,model2,small_to_big = True,first = False):
 			model2_weight = model1_weight[x-xx:,y-yy:]
 		model2_weight_list[counter] = model2_weight
 		counter +=1
-  
-	# else:
-    #  	#adding each model layer weight into lists
-	# 	model1_weight_list = []
-	# 	for name, param in model1.named_parameters():
-	# 		model1_weight_list.append(param.clone().detach())
-	# 	model2_weight_list = []
-	# 	for name,param in model2.named_parameters():
-	# 		model2_weight_list.append(param.clone().detach())
-	# 	#loading model1 weight into model2 weight block
-	# 	counter = 0
-	# 	for model1_weight, model2_weight in zip(model1_weight_list,model2_weight_list):
-	# 		x,y = model1_weight.shape[0],model1_weight.shape[1]
-	# 		xx,yy = model2_weight.shape[0],model2_weight.shape[1]
-	# 		model2_weight = model1_weight[x-xx:,y-yy:]
-	# 		model2_weight_list[counter] = model2_weight
-	# 		counter +=1
 
 	model2.l1.weight = torch.nn.Parameter(model2_weight_list[0])
 	model2.l2.weight = torch.nn.Parameter(model2_weight_list[1])
