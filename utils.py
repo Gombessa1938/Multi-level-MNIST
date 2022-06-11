23a9d7988c3f3a1c80873f7511cfd77ce27c237b
Joe Hua <infomrhua@gmail.com>
2022-06-10 20:46:31 -0700

    bug fix

--- utils.py
+++ utils.py
@@ -47,9 +47,7 @@ def load_model_weight(model1,model2,small_to_big = True,first = False):
 		for model1_weight, model2_weight in zip(model1_weight_list,model2_weight_list):
 			x,y = model1_weight.shape[0],model1_weight.shape[1]
 			xx,yy = model2_weight.shape[0],model2_weight.shape[1]
-			if first:
-				model2_weight = torch.zeros_like(model2_weight)
-			model2_weight = model1_weight[xx-x:,yy-y:]
+			model2_weight = model1_weight[x-xx:,y-yy:]
 			model2_weight_list[counter] = model2_weight
 			counter +=1
 
@@ -58,17 +56,3 @@ def load_model_weight(model1,model2,small_to_big = True,first = False):
 		model2.l3.weight = torch.nn.Parameter(model2_weight_list[2])
 		model2.l4.weight = torch.nn.Parameter(model2_weight_list[3])
 		model2.l5.weight = torch.nn.Parameter(model2_weight_list[4])
-     
-     
-     
-     
-     
-     
-		new_small_l1_weight = model1.l1.weight.clone().detach()
-		new_l1_weight = new_small_l1_weight[:,1024:1024+256]
-		model_new_small = model2
-		model_new_small.l1.weight = torch.nn.Parameter(new_l1_weight)
-		model_new_small.l2.weight = torch.nn.Parameter(model1.l2.weight.clone().detach())
-		model_new_small.l3.weight = torch.nn.Parameter(model1.l3.weight.clone().detach())
-		model_new_small.l4.weight = torch.nn.Parameter(model1.l4.weight.clone().detach())
-		model_new_small.l5.weight = torch.nn.Parameter(model1.l5.weight.clone().detach())
