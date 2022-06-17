fa198653d45902cfbae3d245ac89936b2a3a8f89
Joe Hua <infomrhua@gmail.com>
2022-06-16 23:10:39 -0700

    plot

--- models.py
+++ models.py
@@ -36,20 +36,4 @@ class small(torch.nn.Module):
       x = self.ReLU(layers(x))
     return x
   
-  
-class tiny(torch.nn.Module):
-  def __init__(self,coarse_size=64,l=[64,32,16,16]):
-    super(tiny,self).__init__()
-    self.linears = nn.ModuleList()
-    self.linears.append(nn.Linear(coarse_size,coarse_size//2,bias=False))
-    for i in range(len(l)-1):
-      self.linears.append(nn.Linear(l[i],l[i+1],bias=False))
-    self.linears.append(nn.Linear(l[-1],1,bias=False))
-    self.ReLU = nn.ReLU()
-    
-  def forward(self,x):
-    for layers in self.linears:
-      x = self.ReLU(layers(x))
-    return x
-   
    
