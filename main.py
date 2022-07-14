355d5d5e7a8e272047503894399c1d2cd4550864
Joe Hua <infomrhua@gmail.com>
2022-07-14 09:43:18 -0700

    .

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from train import train
 from utils import load_model_weight
 import config
 from matplotlib import pyplot as plt
+from tqdm import trange 
+
 
 #data loading
 data = np.load('/Users/joe/Documents/llnl_branch/llnl.npz')
@@ -30,61 +32,52 @@ def cycle_train(epoch1,epoch2,epoch3,cycle,loss,res):
     bs = 128
     position = []
     l,r = 0,35
+    
+    res = []
     for i in range(cycle):
         load_model_weight(model1,model2,small_to_big=False)
         optim = torch.optim.Adam(model1.parameters(), lr=0.001)
-        res = train(model1,loss,optim,down_sampled_train_small,label,epoch1,bs,device,res)
-        position  = np.arange(l,r)
-        plt.plot(position,res,'b')
-        l  = r
-        r +=10
+        res_ = train(model1,loss,optim,down_sampled_train_small,label,epoch1,bs,device,res)
+        res += res_
+        # position  = np.arange(l,r)
+        # plt.plot(position,res,'b')
+        # l  = r
+        # r +=10
         
         
         model2 = medium()
         load_model_weight(model1,model2,small_to_big=True,first = True)
         optim = torch.optim.Adam(model2.parameters(), lr=0.001)
-        res = train(model2,loss,optim,concat_train_medium,label,epoch2,bs,device,res)
-        position = np.arange(l,r)
-        plt.plot(position,res,'g')
-        l = r
-        r +=10
+        res_ = train(model2,loss,optim,concat_train_medium,label,epoch2,bs,device,res)
+        res += res_
+        # position = np.arange(l,r)
+        # plt.plot(position,res,'g')
+        # l = r
+        # r +=10
         
         model3 = Large()
         load_model_weight(model2,model3,small_to_big=True,first = True)
         optim = torch.optim.Adam(model2.parameters(), lr=0.0001)
-        res = train(model3,loss,optim,concat_train_large,label,epoch2,bs,device,res)
-
-        position = np.arange(l,r)
-        plt.plot(position,res,'r')
-        
-
-
-        # model2 = medium()
-        # load_model_weight(model3,model2,small_to_big=False,first = True)
-        # optim = torch.optim.Adam(model2.parameters(), lr=0.0001)
-        # res = train(model2,loss,optim,concat_train_medium,label,epoch2,bs,device,res)
-
+        res_ = train(model3,loss,optim,concat_train_large,label,epoch2,bs,device,res)
+        res += res_
         # position = np.arange(l,r)
-        # plt.plot(position,res,'g')
-        # l +=50
-        # r +=50
-        # model1 = small()
-        # load_model_weight(model2,model1,small_to_big=False,first= True)
-        # optim = torch.optim.Adam(model1.parameters(), lr=0.0001)
-        # res = train(model1,loss,optim,down_sampled_train_small,label,epoch3,bs,device,res)
-        
-        # position = np.arange(l,r)
-        # plt.plot(position,res,'b')        
-        # l +=50
-        # r +=50
-    plt.ylabel('loss')
-    plt.xlabel('iterations')
-    plt.plot(0,0,'b',label='small network')
-    plt.plot(0,0,'g', label = 'medium network')
-    plt.plot(0,0,'r',label='large network')
-    plt.legend(loc='upper right')
-    plt.show()
+        # plt.plot(position,res,'r')
+
     return res
+
+
+for i in trange()
+
+
+
+plt.ylabel('loss')
+plt.xlabel('iterations')
+plt.plot(0,0,'b',label='small network')
+plt.plot(0,0,'g', label = 'medium network')
+plt.plot(0,0,'r',label='large network')
+plt.legend(loc='upper right')
+plt.show()
+
       
 result = []
 out = cycle_train(35,10,10,cycle=1,loss=loss,res =result)
